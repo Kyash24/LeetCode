bfa4059e9e3698be940ea336317fee07b1a663b1
Yash Kumar <151900244+Kyash24@users.noreply.github.com>
2026-07-30 22:40:24 +0530

    Time: 2 ms (7.24%), Space: 44.6 MB (69.87%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int pivot = -1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if (pivot == -1) {
+            Arrays.sort(nums);
+            return;
+        }
+
+        for (int i = n - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                int temp = nums[i];
+                nums[i] = nums[pivot];
+                nums[pivot] = temp;
+                break;
+            }
+        }
+
+        Arrays.sort(nums, pivot + 1, n);
+    }
+}
